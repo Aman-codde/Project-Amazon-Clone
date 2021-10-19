61636f56de2b1ba70e971347f90b3d7f12169941
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-19 07:09:47 -0700

    feat(server): create restful api for creating new product

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,11 @@
 
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import { UserModel } from './schemas/user.schema.js'
 import mongoose from 'mongoose';
+import { ProductModel } from './schemas/product.schema.js';
 
 const app = express();
-const __dirname = path.resolve();
 const PORT = 3501;
 
 mongoose.connect('mongodb://localhost:27017/amazonCloneDB')
@@ -16,13 +15,26 @@ mongoose.connect('mongodb://localhost:27017/amazonCloneDB')
 .catch(err => console.log('Failed to Connect to DB', err))
 
 
-
 app.use(cors());
 app.use(express.json());
 
 app.get('/', function(req, res) {
    res.json({message:'test'});
 });
+
+// create/add new product
+app.post('/create-product', function(req,res) {
+    const new_product = new ProductModel(req.body);
+    console.log("new product: ", new_product)
+    new_product
+    .save()
+    .then(data => {
+        console.log({data});
+        res.json({data})
+    })
+    .catch(err => res.status(501).json({error: err}))
+})
+
 
 app.get('/users', function(req,res){
     UserModel.find()
@@ -32,6 +44,7 @@ app.get('/users', function(req,res){
         res.json({errors: err});
     })
 });
+
 app.post('/create-user', function(req,res){
     const {name, email, username} = req.body;
     const user = new UserModel({
@@ -39,20 +52,21 @@ app.post('/create-user', function(req,res){
         username,
         email,
     });
-    user.save()
+    user
+    .save()
     .then((data) => {
         res.json({data});
     })
     .catch(err => {
-        res.status(501);
-        res.json({errors: err});
+        res.status(501).json({errors: err});
     })
 });
 
-
 app.delete('/delete-user/:id', function(req, res) {
     const _id = req.params.id;
-    UserModel.findByIdAndDelete(_id).then((data) => {
+    UserModel
+    .findByIdAndDelete(_id)
+    .then((data) => {
         console.log(data);
         res.json({data});
     });
@@ -60,7 +74,8 @@ app.delete('/delete-user/:id', function(req, res) {
 
 app.put('/update-user/:id', function(req, res) {
     console.log("Update user");
-    UserModel.findByIdAndUpdate(
+    UserModel
+    .findByIdAndUpdate(
         req.params.id,
         {
             $set: { name: req.body.name, email: req.body.email },
@@ -78,7 +93,6 @@ app.put('/update-user/:id', function(req, res) {
         }
     )
 })
-
 
 app.listen(PORT, function(){
     console.log( `starting at localhost http://localhost:${PORT}`);
