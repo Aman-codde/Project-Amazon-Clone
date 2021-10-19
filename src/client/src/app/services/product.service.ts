ffd072b66d1eaaa54828113c4d646f6d67159bc3
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-19 07:11:12 -0700

    feat(client): create new service for product

---
+++ src/client/src/app/services/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Product } from '../../../../shared/models/product.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private api: ApiService) 
+  { }
+
+  postProducts(product: Product) {
+    console.log("product service data: ", product);
+    //return this.api.post<Product[],Product>('create-product',product);
+    return this.api.post<{data: Product},Product>('create-product',product);
+  }
+}
