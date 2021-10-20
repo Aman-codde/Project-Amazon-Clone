53e7665866398c4dfab8d258d59f701bcfe6ea4a
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-20 09:06:59 -0700

    fix(server): delete _id property from schema

--- src/server/schemas/category.schema.ts
+++ src/server/schemas/category.schema.ts
@@ -4,7 +4,6 @@ import { Category } from '../../shared/models/category.model.js';
 const {model,Schema} = mongoose;
 
 const categorySchema = new Schema<Category>({
-    _id: {type: mongoose.Types.ObjectId},
     category_name: {type:String, required: true},
     parent_category: {type: mongoose.Types.ObjectId, ref: 'Category'}
 });
--- src/server/schemas/product.schema.ts
+++ src/server/schemas/product.schema.ts
@@ -4,7 +4,6 @@ import { Product } from '../../shared/models/product.model.js';
 const {Schema,model} = mongoose;
 
 const productSchema = new Schema<Product>({
-    _id: {type: mongoose.Types.ObjectId},
     product_name: {type: String, required: true},
     price: {type: Number, required: true},
     quantity: {type: Number, required: true},
