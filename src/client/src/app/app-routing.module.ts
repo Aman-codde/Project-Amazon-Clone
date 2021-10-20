0f095cb5cf8c04feb0c4daabd5630455a29ac7ef
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-20 06:55:04 -0700

    feat(client): add routing paths

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductInputComponent } from './components/product-input/product-input.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { PageUsersComponent } from './pages/page-users/page-users.component';
 
 const routes: Routes = [
   {path: '',component: ProductsListComponent},
+  {path: 'products', component: ProductsListComponent},
   {path: 'users', component: PageUsersComponent},
-  {path:'add-product', component: ProductInputComponent}
+  {path:'add-product', component: ProductInputComponent},
+  {path: 'products/id', component: ProductDetailsComponent}
 ];
 
 @NgModule({
