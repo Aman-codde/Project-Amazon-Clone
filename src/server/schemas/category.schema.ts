dd88a6e3b702f86b07e193f72027fcfd3e74a0f5
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-20 07:18:33 -0700

    fix(shared): add _id property in model & schema

--- src/server/schemas/category.schema.ts
+++ src/server/schemas/category.schema.ts
@@ -1,9 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 import { Category } from '../../shared/models/category.model.js';
 
 const {model,Schema} = mongoose;
 
 const categorySchema = new Schema<Category>({
+    _id: {type: mongoose.Types.ObjectId},
     category_name: {type:String, required: true},
     parent_category: {type: mongoose.Types.ObjectId, ref: 'Category'}
 });
--- src/shared/models/category.model.ts
+++ src/shared/models/category.model.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 export interface Category {
-    _id: mongoose.Types.ObjectId,
+    _id?: mongoose.Types.ObjectId,
     category_name: string,
     parent_category?: mongoose.Types.ObjectId
 }
