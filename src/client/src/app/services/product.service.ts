212a6bb219ad2f024ccdc602ff35b929b1659dc4
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-21 14:10:50 -0700

    feat(client): add product by id service

--- src/client/src/app/services/product.service.ts
+++ src/client/src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Product } from '../../../../shared/models/product.model'
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,12 +21,9 @@ export class ProductService {
     return this.api.post<{data:Product[]},{categories: string}>('products',{categories}).pipe(tap(d => console.log("product list: ",d)));
   }
 
-  /*getProducts(params: string) {
-    return this.api.post<{data:Product[]},{_id: string}>('products',{_id: params});
-  }*/
-
-  /*getProduct(params: string) {
-    return this.api.post<{data:Product[]},{_id: string}>('products',{_id: params});
-  }*/
+  getProduct(productId: string) {
+    console.log(productId);
+    return this.api.post<{data: Product},{productId: string}>('product', {productId}).pipe(map( res => res.data));
+  }
 
 }
