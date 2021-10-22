fbe5386817178b834207803f0854b0b4450a64e4
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-21 17:26:49 -0700

    fix(client): fix route value

--- src/client/src/app/services/product.service.ts
+++ src/client/src/app/services/product.service.ts
@@ -21,9 +21,8 @@ export class ProductService {
     return this.api.post<{data:Product[]},{categories: string}>('products',{categories}).pipe(tap(d => console.log("product list: ",d)));
   }
 
-  getProduct(productId: string) {
-    console.log(productId);
-    return this.api.post<{data: Product},{productId: string}>('product', {productId}).pipe(map( res => res.data));
+  getProduct(_id: string) {
+    return this.api.post<{data: Product},{_id: string}>('product/'+_id, {_id})
   }
 
 }
