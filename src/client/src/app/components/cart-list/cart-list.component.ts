200327b62421d551014b5fa9604a816456a05bd4
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-25 15:35:50 -0500

    feat(client): add a feature to show all carts

---
+++ src/client/src/app/components/cart-list/cart-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
+import { Cart } from '../../../../../shared/models/cart.model';
+
+@Component({
+  selector: 'app-cart-list',
+  templateUrl: './cart-list.component.html',
+  styleUrls: ['./cart-list.component.scss']
+})
+export class CartListComponent implements OnInit {
+  cart$ : Observable<Cart[]>
+  constructor(private apiService: ApiService) 
+  { 
+    this.cart$ = this.apiService.get<Cart[]>('cart')
+  }
+
+  ngOnInit(): void {
+     
+  }
+
+}
