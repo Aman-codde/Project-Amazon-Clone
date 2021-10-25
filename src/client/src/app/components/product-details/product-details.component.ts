65780b8d69131efde86c0e17be2453d2191184ee
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-25 15:02:12 -0500

    feat(client): (without ngrx) add item into the cart in services & component

--- src/client/src/app/components/product-details/product-details.component.ts
+++ src/client/src/app/components/product-details/product-details.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
 import { AppState } from 'src/app/store';
 import { loadProduct} from 'src/app/store/actions/product/product.actions';
 import { productSelector, selectedProductSelector } from 'src/app/store/selectors/product/product.selectors';
 import { Product } from '../../../../../shared/models/product.model';
+import { User } from '../../../../../shared/models/user.model';
 
 @Component({
   selector: 'app-product-details',
@@ -16,10 +18,13 @@ export class ProductDetailsComponent implements OnInit {
   product$: Observable<Product | null>;
   selectedProduct$: Observable<Product | null>
   selectedProduct : Product| null = null; // new
+  //temporary
+  //selectedUser: Observable<User | null>; 
 
   constructor(
     private route: ActivatedRoute,
     private store: Store<AppState>,
+    private cartService: CartService
   ) 
   { 
     this.product$ = this.store.select(productSelector)
@@ -39,6 +44,18 @@ export class ProductDetailsComponent implements OnInit {
       return false;
     else
       return true;
+  }
+
+  addToCart() {
+    // if new user(if userid not found in  the cart collection)
+      // 1.create cart and 2.add product to cart
+
+    // if existing user(if userid found in  the cart collection)
+      // 2. pass userid and add product to card
+    //this.cartService.selectUser("615ee77596fadd70d45456a2");
+    const userId = "615ee77596fadd70d45456a2";
+    const product = this.selectedProduct!;
+    this.cartService.updateCart(userId, product).subscribe();
   }
   
 }
