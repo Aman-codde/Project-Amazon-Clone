d7d14cffbc0075edcf66f5768c3550edbd8f5804
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-11-02 09:21:03 -0500

    feat(client): add feature 'update cart' using store in ngrx

--- src/client/src/app/components/product-details/product-details.component.ts
+++ src/client/src/app/components/product-details/product-details.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { CartService } from 'src/app/services/cart.service';
 import { AppState } from 'src/app/store';
+import { updateCart } from 'src/app/store/actions/cart/cart.actions';
 import { loadProduct} from 'src/app/store/actions/product/product.actions';
 import { productSelector, selectedProductSelector } from 'src/app/store/selectors/product/product.selectors';
 import { Product } from '../../../../../shared/models/product.model';
-import { User } from '../../../../../shared/models/user.model';
+
 
 @Component({
   selector: 'app-product-details',
@@ -17,14 +17,10 @@ import { User } from '../../../../../shared/models/user.model';
 export class ProductDetailsComponent implements OnInit {
   product$: Observable<Product | null>;
   selectedProduct$: Observable<Product | null>
-  selectedProduct : Product| null = null; // new
-  //temporary
-  //selectedUser: Observable<User | null>; 
+  selectedProduct : Product| null = null; 
 
   constructor(
-    private route: ActivatedRoute,
     private store: Store<AppState>,
-    private cartService: CartService
   ) 
   { 
     this.product$ = this.store.select(productSelector)
@@ -34,7 +30,7 @@ export class ProductDetailsComponent implements OnInit {
   ngOnInit(): void {
 
     this.selectedProduct$.subscribe(data => this.selectedProduct = data);
-    const id = ""+this.selectedProduct?._id
+    const id = "" + this.selectedProduct?._id 
     this.store.dispatch(loadProduct({data: id}))
 
   }
@@ -47,15 +43,8 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   addToCart() {
-    // if new user(if userid not found in  the cart collection)
-      // 1.create cart and 2.add product to cart
-
-    // if existing user(if userid found in  the cart collection)
-      // 2. pass userid and add product to card
-    //this.cartService.selectUser("615ee77596fadd70d45456a2");
-    //const userId = "615ee77596fadd70d45456a2";
     const product = this.selectedProduct!;
-    this.cartService.updateCart(product).subscribe();
+    this.store.dispatch(updateCart({data: product}));
   }
   
 }
