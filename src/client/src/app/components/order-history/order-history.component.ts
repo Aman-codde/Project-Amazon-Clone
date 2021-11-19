b603d4d248cec9d48b44e2d1e146f3a2100d9623
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-11-19 01:26:32 -0800

    feat(client): add feature to show orders of active user

--- src/client/src/app/components/order-history/order-history.component.ts
+++ src/client/src/app/components/order-history/order-history.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store';
+import { loadOrders } from 'src/app/store/actions/order/order.actions';
+import { ordersSelector } from 'src/app/store/selectors/order/order.selectors';
+import { Order } from '../../../../../shared/models/order.model';
 
 @Component({
   selector: 'app-order-history',
@@ -6,10 +13,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-history.component.scss']
 })
 export class OrderHistoryComponent implements OnInit {
-
-  constructor() { }
+  orders$!: Observable<Order[]> 
+  constructor(
+    private router: Router,
+    private store: Store<AppState>
+  ) 
+  { 
+    this.store.dispatch(loadOrders())
+    this.orders$ = this.store.select(ordersSelector)
+  }
 
   ngOnInit(): void {
+  }
+
+  backToAccount() {
+    this.router.navigate(['/account']);
+  }
+
+  getDateFormat( dateString: any) {
+    return new Date(dateString).toLocaleDateString()
   }
 
 }
