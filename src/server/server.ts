ea57770765df7a004f8f0a70359d2dd1c294203d
Aman <amandeepk221@gmail.com>
2021-12-01 22:51:48 -0800

    feat(server): populate quantity and user in update cart api

--- src/server/server.ts
+++ src/server/server.ts
@@ -308,7 +308,8 @@ app.put('/api/update-cart', authHandler ,function(req:any,res){
         {user: loggedUser._id},
         "count total_amount"
     )
-    .populate('products.product') 
+    .populate('user')
+    .populate('products.product products.selected_quantity') 
     .then(cart => {
         console.log("....cart: ", cart);
         if(cart) {
@@ -338,7 +339,8 @@ app.put('/api/delete-from-cart/:productId', authHandler,function(req:any,res) {
         {$pull: {'products': {product: productId} } },
         {new: true}
     )
-    .populate('products')
+    .populate('user')
+    .populate('products.product')
     .then(data => {
         console.log("delete from cart: ",data);
         res.json({data})
