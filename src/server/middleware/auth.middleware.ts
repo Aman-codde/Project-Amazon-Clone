05175809c7388e3bdf62a40286e59a9bc7c6df97
Aman <amandeepk221@gmail.com>
2022-03-30 21:35:53 -0700

    feat(client): create middleware to show if user is loggedin or not

--- src/server/middleware/auth.middleware.ts
+++ src/server/middleware/auth.middleware.ts
@@ -25,4 +25,20 @@ function authHandle(req: AuthRequest, res: Response, next: NextFunction) {
     next();
 }
 
+export function isLoggedInHandle(req: AuthRequest, res: Response, next: NextFunction) {
+    const cookie = req.cookies['jwt'];
+    console.log('auth ',cookie);
+    
+    jwt.verify(
+        cookie, 
+        process.env.ACCESS_TOKEN_SECRET as string, 
+        (err: any, result: any) => {
+        console.log('Auth result = ', result)
+        if(result) {
+            req.user = result.user;
+        }
+    })
+    next();
+}
+
 export const authHandler = authHandle 
